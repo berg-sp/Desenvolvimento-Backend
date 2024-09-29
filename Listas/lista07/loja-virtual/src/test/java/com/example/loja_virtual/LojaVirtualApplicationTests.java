947c5058ae2d52e342138b41a47bfb17cb98e5db
batistaberg <batista.berg@gmail.com>
2024-09-29 09:26:22 -0300

    Envio Lista07-back

---
+++ Listas/lista07/loja-virtual/src/test/java/com/example/loja_virtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
